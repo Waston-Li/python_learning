ea4683453904f9ced59e0ae2cc3ea9c1d0be77b1
Waston-Li <18222656146@163.com>
2020-01-13 16:12:00 +0800

    测试pycharm的git

--- playList.py
+++ playList.py
@@ -9,13 +9,15 @@ lis1+=['末尾元素'] #+=添加元素,或拼接列表
 del lis1[0]; print("使用del删除元素")
 print("使用.pop()弹出列表末尾最后一个元素，之后删除这个元素:"+lis1.pop())
 #.pop()可以也删除列表中的任何位置的元素，只需在括号中指定索引即可
-#根据值删除元素.remove
-moto=['ducati','yamaha','suziki']
-too_expensive='ducati' ; moto.remove(too_expensive) #也可以写为moto.remove('ducati')
+
+#根据值删除元素.remove()
+moto_car=['ducati','yamaha','suziki']
+too_expensive='ducati' ; moto_car.remove(too_expensive) #也可以写为moto.remove('ducati')
 
 #组织列表
-moto.sort()  #sorted(x)
-print('.sort()使表中元素按字母顺序排列:',end=""); print(moto)
-#此方法修改表内元素顺序 ，若想不修改的按顺序输出可以使用 sorted(x)
+print('sorted()使表中元素按字母顺序排列:',end=""); print(sorted(moto_car))
+# .sort()  此方法修改表内元素顺序 ，若想不修改的按顺序输出可以使用 sorted(x)
 # .sort(reverse=True)可以按字母反序排列表中的元素
-print("len()返回列表长度："+str(len(moto)))
+print("len()返回列表长度："+str(len(moto_car)))
+
+#测试下pycharm的git是否成功

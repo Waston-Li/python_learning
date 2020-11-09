81423f5d5a8d2e757fc492042236f26a981eee4c
WastonLi <18222656146@163.com>
2020-11-09 16:55:55 +0800

    Delete tensor_test.py

--- tensor_test.py
+++
@@ -1,7 +0,0 @@
-import tensorflow as tf
-
-print(tf.__version__)
-tf.compat.v1.disable_eager_execution() #使tensor2.0版本兼容1.0的sess
-hello = tf.constant('Hello, TensorFlow!')
-sess = tf.compat.v1.Session()
-print(sess.run(hello))
